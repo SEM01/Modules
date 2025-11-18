8b60fbb11de0c2732636ab933f2f5078553d462c
SEM01 <195654711+SEM01@users.noreply.github.com>
2025-11-18 08:27:45 -0600

    Update decorators.py

--- decorators.py
+++ decorators.py
@@ -16,5 +16,19 @@ def timer(func):
 
     return wrapper_timer
 
+def debug(func):
+    """Print the function signature and return value"""
+
+    @functools.wraps(func)
+    def wrapper_debug(*args, **kwargs):
+        args_repr = [repr(a) for a in args]
+        kwargs_rper = [f"{k}]{repr(v)}" for k, v in kwargs.items()]
+        signature = ", ".join(args_repr + kwargs_rper)
+        print(f"Calling {func.__name__}({signature})")
+        value = func(*args, **kwargs)
+        print(f"{func.__name__}() returned {repr(value)}")
+        return value
+
+    return wrapper_debug
 
 
