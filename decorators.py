8a23a6d8dbdc12e8515e463de76fae85615b5d24
SEM01 <195654711+SEM01@users.noreply.github.com>
2025-11-18 08:45:56 -0600

    Update decorators.py

--- decorators.py
+++ decorators.py
@@ -31,4 +31,13 @@ def debug(func):
 
     return wrapper_debug
 
+def slow_down(func):
+    """Sleep 1 second before calling the function"""
+
+    @functools.wraps(func)
+    def wrapper_slow_down(*args, **kwargs):
+        time.sleep(1)
+        return func(*args, **kwargs)
+
+    return wrapper_slow_down
 
